e21c5bd3aab5e0832ff14b64be2cbc0251ab8ae8
Thomas Crha <thomas@9bitbyte.com>
2021-10-07 17:58:55 +1100

    added interface variable to set an interface to set the ip for

--- devel/start.sh
+++ devel/start.sh
@@ -2,5 +2,6 @@
 # Useful for running locally and doing development/testing
 export FLASK_APP=jackcast
 export FLASK_ENV=development
-ip4=$(/sbin/ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
+INTERFACE=wlan0
+ip4=$(/sbin/ip -o -4 addr list $INTERFACE | awk '{print $4}' | cut -d/ -f1)
 flask run --host=$ip4 --port=5000
